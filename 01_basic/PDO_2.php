11ca0e929449e022f948d30ab0e2209e0e85aad1
Piyush Kumar Raikwar <94155141+Piyush289kumar@users.noreply.github.com>
2023-10-26 11:21:29 +0000

    PDO_testing

---
+++ 01_basic/PDO_2.php
@@ -0,0 +1,15 @@
+<?php
+$db_name = "mysql:host=localhost;dbname=art_db";
+$userName = "root";
+$pwd = "";
+$conn = new PDO($db_name, $userNamem, $pwd);
+$sql = $conn->prepare("SELECT * FROM post");
+$sql->execute();
+$result = $sql->fetchAll(PDO::FETCH_ASSOC);
+if (count($result) > 0) {
+    foreach ($result as $row) {
+        echo $row['post_id'] . " - " .  $row['title'] .  " - " . $row['author'] . "<br>";
+    }
+} else {
+    echo "NO RECORD FOUND.....";
+}
