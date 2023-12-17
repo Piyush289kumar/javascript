d6636bc189383a10f34ca8f64a3ade159378a2ce
Piyushkumar289 <piyushraikwar289@gmail.com>
2023-12-17 21:00:00 +0530

    Loops Conscept

---
+++ 03_basics/03_loops.js
@@ -0,0 +1,12 @@
+// for...of loops
+// const array1 = ['a', 'b', 'c'];
+
+// for (const index of array1) {
+//     console.log(index);
+// }
+
+const iterable = [10, 20, 30];
+
+for(const index of iterable){
+    console.log(index + 1);
+}
