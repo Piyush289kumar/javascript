da2564e738ba45ebbaf1fdf414436cb19ee775a1
Piyushkumar289 <piyushraikwar289@gmail.com>
2023-12-18 13:00:38 +0530

    forEach Loop try

--- 03_basics/03_loops.js
+++ 03_basics/03_loops.js
@@ -5,8 +5,14 @@
 //     console.log(index);
 // }
 
-const iterable = [10, 20, 30];
+// const iterable = [10, 20, 30];
 
-for(const index of iterable){
-    console.log(index + 1);
-}
+// for(const index of iterable){
+//     console.log(index + 1);
+// }
+
+const myArray = [1, 2, 3, 4, 5, 6, 7];
+
+myArray.forEach((items) => {
+  console.log(items * 2);
+});
