bd44c192287c922a96de140f9d4c105250d12a92
Piyushkumar289 <piyushraikwar289@gmail.com>
2023-12-20 10:15:53 +0530

    Post On LinkedIn Q1

--- exercies/Q1.js
+++ exercies/Q1.js
@@ -4,18 +4,25 @@
 
 // Solution =======
 
-// Get the current date and time 
+// Get the current date and time
 let toDay = new Date();
 
 // Get the day of the week (0-6, Where 0 is Sunday and 6 is Saturday)
 let day = toDay.getDay();
 
 // Array of day names
-let dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let dayList = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 // Display the current day
 console.log(`Today is : ${dayList[day]}.`);
-
 
 // Get the current hour, minute and second
 
@@ -23,22 +30,21 @@ let hour = toDay.getHours();
 let minute = toDay.getMinutes();
 let second = toDay.getSeconds();
 
-
 // Determine if it's AM or PM
-let prepand = (hour >= 12) ? "PM" : "AM";
+let prepand = hour >= 12 ? "PM" : "AM";
 
 // Convert 24-hour format to 12-hour format
-hour = (hour =>12) ? hour - 12 : hour;
+hour = ((hour) => 12) ? hour - 12 : hour;
 
 // Check for special cases when hour is 0
 if (hour === 0 && prepand === "PM") {
-    if (minute === 0 && second === 0) {
-        hour : 12;
-        prepand = 'Noon';
-    } else {
-        hour = 12;
-        prepand = "PM";
-    }
+  if (minute === 0 && second === 0) {
+    hour: 12;
+    prepand = "Noon";
+  } else {
+    hour = 12;
+    prepand = "PM";
+  }
 }
 
 // Display the current time
