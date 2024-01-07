3114a73f216e22056f764748d2b69b9a528274df
Piyush Kumar Raikwar <94155141+Piyush289kumar@users.noreply.github.com>
2024-01-07 21:49:05 +0530

    WAP to test whether a string ends with "Script". The string length must be greater than or equal to 6.

--- exercies/Q60.js
+++ exercies/Q60.js
@@ -1,33 +1,39 @@
 /* Question 60: WAP to test whether a string ends with "Script". The string length must be greater than or equal to 6.
 Solution: */
 
-function checkLastString(strParam) {
-    
-    try {
-        if (strParam.length < 6) {        
-            throw ("Length is Greater Then 6 is Must");
-        }    
-        try {
-            if (strParam.slice(-6) === 'script') {
-             console.log(`Found: ${strParam}`);   
-             return true;
-            } else {
-                throw("Not Found");
-            }
-             
-        } catch (error) {
-            console.log(`Inner Error: ${error}`);
-            return false;
-        }
+// Method 1: 
+// function checkLastString(strParam) {
+//     try {
+//         if (strParam.length < 6) {        
+//             throw ("Length is Greater Then 6 is Must");
+//         }    
+//         try {
+//             if (strParam.slice(-6) === 'script') {
+//              console.log(`Found: ${strParam}`);   
+//              return true;
+//             } else {
+//                 throw("Not Found");
+//             }
+//         } catch (error) {
+//             console.log(`Inner Error: ${error}`);
+//             return false;
+//         }
+//     } catch (error) {
+//         console.log(`Error : ${error}`);
+//         return false;
+//     }    
+// }
+// // checkLastString('java');
+// console.log(checkLastString('java'));
+// console.log(checkLastString('javasdfdfdf'));
+// console.log(checkLastString('javascript'));
+// console.log(checkLastString('java script'));
+// console.log(checkLastString('java scripts'));
 
-    } catch (error) {
-        console.log(`Error : ${error}`);
-        return false;
-        
-    }    
+// Method 2: ES6
+
+function endStrCheck(strParam) {
+    return strParam.endsWith('script');
 }
 
-// checkLastString('java');
-console.log(checkLastString('java'));
-console.log(checkLastString('javasdfdfdf'));
-console.log(checkLastString('javascript'));
+console.log(endStrCheck('javascript'));
