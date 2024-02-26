ce6da0962dea02883dae638e7e8d3c7e6583ed89
Piyush289kumar <piyushraikwar289@gmail.com>
2024-02-26 18:12:47 +0530

    WAP to divide a given array of positive integers into two parts. First element belongs to the first part, second element belongs to the second part, and third element belongs to the first part and so on. Now compute the sum of two parts and store it in an array of size two.

--- exercies/Q79.js
+++ exercies/Q79.js
@@ -1,4 +1,4 @@
-/* Question 79: WAP to divide a given array of positive integers into two parts. First element belongs to the first part, second element belongs to the second part, and third element belongs to the first part and so on. Now compute the sum of two parts and store it in an array of size two. */
+/* Question 79: WAP to divide a given array of positive integers into two parts. First element belongs to the first part, second element belongs to the second part, and third element belongs to the first part and so on. Now compute the sum of two parts and store it in an array of size two.*/
 
 // Method 2:
 
