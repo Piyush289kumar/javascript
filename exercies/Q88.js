190c5df81ce2dfaf549893be405a75d751218634
Piyush289kumar <piyushraikwar289@gmail.com>
2024-06-11 10:45:47 +0530

    Create a Boilder Question

---
+++ exercies/Q88.js
@@ -0,0 +1,4 @@
+/* Question 88: Make an Object has only property/value which are Common on Input Objects. */
+
+const input1 = { a: 1, b: 2, c: 3, d: 10, e: 12 };
+const input2 = { a: 2, e: 12, f: 6, d: 10 };
